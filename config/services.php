072f174e5f3c18404c286eb295958b93acacb40e
Masood <masood.vahid@gmail.com>
2026-09-10 11:29:10 +0330

    feat: configure Kavenegar health thresholds

--- config/services.php
+++ config/services.php
@@ -4,5 +4,7 @@ return [
         'api_key' => env('KAVENEGAR_API_KEY'),
         'otp_template' => env('KAVENEGAR_OTP_TEMPLATE', 'tukahr-login'),
         'approval_template' => env('KAVENEGAR_APPROVAL_TEMPLATE', 'tukahr-approval'),
+        'low_credit_threshold' => (int) env('KAVENEGAR_LOW_CREDIT_THRESHOLD', 100000),
+        'timeout_seconds' => (int) env('KAVENEGAR_TIMEOUT_SECONDS', 8),
     ],
 ];
