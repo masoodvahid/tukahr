77be2643e99fd313cbff215560306107b3843689
Masood <masood.vahid@gmail.com>
2026-09-10 11:26:31 +0330

    feat: allow system admin through HR manager gates

--- app/Http/Middleware/EnsureHrManager.php
+++ app/Http/Middleware/EnsureHrManager.php
@@ -1,4 +1,15 @@
 <?php
 namespace App\Http\Middleware;
-use Closure;use Illuminate\Http\Request;
-class EnsureHrManager { public function handle(Request $request, Closure $next){ abort_unless($request->user()?->hasRole('hr_manager'),403); return $next($request); } }
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsureHrManager
+{
+    public function handle(Request $request, Closure $next)
+    {
+        abort_unless($request->user() && ($request->user()->isSystemAdmin() || $request->user()->hasRole('hr_manager')), 403);
+
+        return $next($request);
+    }
+}
